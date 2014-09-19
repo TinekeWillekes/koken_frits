553cd3c7898d244359d129e638e17ba0c7abf074
Tineke Willekes <tinekewillekes@hotmail.com>
2014-09-19 17:38:19 +0200

    recipes controller and views added

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,4 +5,49 @@ class RecipesController < ApplicationController
     @recipes = current_user.recipes.all
   end
   
+  def show
+    @recipe = current_user.recipes.find(params[:id])
+  end
+  
+  def new
+    @recipe = Recipe.new
+  end
+  
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
+    if @recipe.save
+      flash.now[:success] = "Added new recipe"
+      redirect_to recipes_path
+    else
+      flash.now[:error] = "Error"
+      render "new"
+    end
+  end
+  
+  def edit
+    @recipe = current_user.recipes.find(params[:id])
+  end
+  
+  def update
+    @recipe = current_user.recipes.find(params[:id])
+    if @recipe.update_attributes(recipe_params)
+      flash.now[:success] = "Recipe updated"
+      redirect_to recipes_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    current_user.recipes.find(params[:id]).destroy
+    flash[:success] = "Recipe deleted."
+    redirect_to recipes_path
+  end
+  
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title)
+  end
+  
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,6 @@ class Recipe < ActiveRecord::Base
   belongs_to :user
   
   validates :user_id, presence: true
-  validates :title, length: { minimum: 20 }, presence: true
+  validates :title, length: { maximum: 30 }, presence: true
 
 end
