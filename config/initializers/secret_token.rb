698f999e855378b6f85e84daa359bac4378fec12
Tineke Willekes <tinekewillekes@hotmail.com>
2014-09-19 12:57:26 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KokenFrits::Application.config.secret_key_base = '063beae521862dc582c4dcc1cd4fd883b635f1a87e2bd68747fcfb91a1145f21913065bcae37ad84f57696097ce46f20c48b2caeea722f2b6b23b267a46accb7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KokenFrits::Application.config.session_store :cookie_store, key: '_koken_frits_session'
